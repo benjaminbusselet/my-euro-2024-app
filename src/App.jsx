a5de42e13f5b7088dc27a93553e2306e2305518b
benjaminbusselet <benjamin.busselet@icloud.com>
2024-06-09 22:19:25 +0200

    edit build

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import FinalStage from './pages/FinalStage';
 import Navbar from './components/Navbar';
 import { ResultsProvider } from './ResultsContext';
 import './App.css';
 
-const App = () => {
-  return (
-    <ResultsProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/final-stage" element={<FinalStage />} />
-        </Routes>
-      </Router>
-    </ResultsProvider>
-  );
-};
+const App = () => (
+  <ResultsProvider>
+    <Router basename="/projects/euro2024predicator">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/final-stage" element={<FinalStage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  </ResultsProvider>
+);
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,14 @@
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-title">Euro 2024 Predictor</div>
-            <div className="navbar-links">
-                <Link to="/">Phase de groupe</Link>
-                <Link to="/final-stage">Phase finale</Link>
-            </div>
-        </nav>
-    );
-};
+const Navbar = () => (
+    <nav className="navbar">
+        <div className="navbar-title">Euro 2024 Predictor</div>
+        <div className="navbar-links">
+            <Link to="/">Phase de groupe</Link>
+            <Link to="/final-stage">Phase finale</Link>
+        </div>
+    </nav>
+);
 
 export default Navbar;
